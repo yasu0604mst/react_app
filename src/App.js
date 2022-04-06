cb960b05ac68862ad2cdbcbf2e51b6f8fa7ab6fe
yasu0604mst <yakubota2020@gmail.com>
2022-04-06 20:45:08 +0900

    新規作成

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import Test from './Test';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ku Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <Test></Test>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Test.js
@@ -0,0 +1,9 @@
+function Test() {
+  return (
+    <div>
+      test aaa
+    </div>
+  );
+}
+
+export default Test;
